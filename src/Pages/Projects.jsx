7227616bf4af18927017b598832f1597b8224b1a
amanprajapati3 <156068199+amanprajapati3@users.noreply.github.com>
2025-03-20 22:46:12 +0530

    Update Projects.jsx

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -33,7 +33,7 @@ const Projects = () => {
         <div id="project_scroll">
         <Carousel responsive={responsive} className=" rounded-xl p-2 mx-5 border-2 border-gray-300 my-5"  >
           <div className="bg-white rounded-xl hover:shadow-2xl hover:shadow-gray-500 transition-all duration-200 text-center mx-3 md:mx-8 mt-3">
-            <img src="public/Blog_img.png" className="p-1 rounded-xl h-64" alt="" />
+            <img src="/Blog_img.png" className="p-1 rounded-xl h-64" alt="" />
             <h1 className="text-3xl font-semibold py-2">Blog Website</h1>
             <p className="p-2">
               This is a frontend clone of a blog website built using{" "}
@@ -47,7 +47,7 @@ const Projects = () => {
             </button>
           </div>
           <div className="bg-white rounded-xl hover:shadow-2xl hover:shadow-gray-500 transition-all duration-200 text-center mx-3 md:mx-8 mt-3">
-            <img src="public/myntra_img.png" className="p-1 rounded-xl h-64" alt="" />
+            <img src="/myntra_img.png" className="p-1 rounded-xl h-64" alt="" />
             <h1 className="text-3xl font-semibold py-2">Myntra Clone</h1>
             <p className="p-2">
               This is a frontend clone of a Myntra website built using{" "}
@@ -61,7 +61,7 @@ const Projects = () => {
             </button>
           </div>
           <div className="bg-white rounded-xl hover:shadow-2xl hover:shadow-gray-500 transition-all duration-200 text-center mx-3 md:mx-8 mt-3">
-            <img src="public/jarvis_img.png" className="p-1 rounded-xl h-64 w-full" alt="" />
+            <img src="/jarvis_img.png" className="p-1 rounded-xl h-64 w-full" alt="" />
             <h1 className="text-3xl font-semibold py-2">AI Virtual Assistance</h1>
             <p className="p-2">
             I have developed an AI virtual assistant called <strong> Jarvis</strong> using <strong>HTML, CSS, and JavaScript</strong> . It simulates interactive conversations and performs various tasks based on user input. This project showcases my frontend skills and enhances user interaction with a smart, responsive interface.
@@ -71,7 +71,7 @@ const Projects = () => {
             </button>
           </div>
           <div className="bg-white rounded-xl hover:shadow-2xl hover:shadow-gray-500 transition-all duration-200 text-center mx-3 md:mx-8 mt-3">
-            <img src="public/full_stack_blog.png" className="p-1 rounded-xl h-64" alt="" />
+            <img src="/full_stack_blog.png" className="p-1 rounded-xl h-64" alt="" />
             <h1 className="text-3xl font-semibold py-2">Full Stack Blog App</h1>
             <p className="p-2">
             I have developed a MERN stack blog application using the <strong>(MongoDB, Express.js, React, and Node.js)</strong> . This project features a seamless user experience with dynamic content management, authentication, and a responsive design. It showcases my skills in both frontend and backend development.
@@ -82,7 +82,7 @@ const Projects = () => {
           </div>
           <div className="bg-white rounded-xl hover:shadow-2xl hover:shadow-gray-500 transition-all duration-200 text-center mx-3 md:mx-8 mt-3">
             <center>
-            <img src="public/calculator_img.png" className="p-1 rounded-xl h-64" alt="" />
+            <img src="/calculator_img.png" className="p-1 rounded-xl h-64" alt="" />
             </center>
             <h1 className="text-3xl font-semibold py-2">Calculator</h1>
             <p className="p-2">
@@ -93,7 +93,7 @@ const Projects = () => {
             </button>
           </div>
           <div className="bg-white rounded-xl hover:shadow-2xl hover:shadow-gray-500 transition-all duration-200 text-center mx-3 md:mx-8 mt-3">
-            <img src="public/todo_img.png" className="p-1 rounded-xl h-64" alt="" />
+            <img src="/todo_img.png" className="p-1 rounded-xl h-64" alt="" />
             <h1 className="text-3xl font-semibold py-2">ToDo List</h1>
             <p className="p-2">
             I have built a fully functional <strong>To Do List</strong> using <strong>HTML, CSS, and JavaScript</strong> . It features a user-friendly interface with essential arithmetic operations and real-time calculations. This project enhances my frontend development skills and demonstrates interactive UI design..
